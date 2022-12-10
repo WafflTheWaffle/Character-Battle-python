6c6d45eab4a5f26ff061f10ba70ef651e465ac45
Waffl <120218803+WafflTheWaffle@users.noreply.github.com>
2022-12-10 14:03:43 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+import time
+
 HAMSTER = {
     "hp": 27,
     "color": "Mint",
@@ -18,29 +20,52 @@ hamster_rival = {
 
 print(HAMSTER)
 print(HAMSTER["hp"])
-
+time.sleep(1)
 HAMSTER["hp"] = HAMSTER["hp"] + 20
 print(HAMSTER["hp"])
-
+time.sleep(1)
 print(hamster_rival)
 print(hamster_rival["hp"])
 hamster_rival["hp"] = hamster_rival["hp"] + 20
 print(hamster_rival["hp"])
+time.sleep(1)
+
+
+# This code for enemy attack is not working right now.
+def enemy_attack():
+    if hamster_rival["hp"] < 0:
+        exit
+
+    HAMSTER["hp"] = HAMSTER["hp"] - 10
+    print("\nThe current hamster hp is now")
+    print(HAMSTER["hp"])
+
 
 def attack():
+    if HAMSTER["hp"] < 0:
+        exit
+
     if hamster_rival["hp"] < 0:
         hamster_rival["hp"] = 0
     user_attack = input("Do you want to attack? ")
     user_attack = str(user_attack)
-    if user_attack := "Yes":
-        hamster_rival["hp"] = hamster_rival["hp"] - 20
-    else:
+    if user_attack != "y":
         print("What a coward!")
-
-    print("\nThe current hamster rival hp is now")
+    else:
+        hamster_rival["hp"] = hamster_rival["hp"] - 20
+    print("Great! ", end='')
+    print("The current hamster rival hp is now ", end='')
     print(hamster_rival["hp"])
 
-while(hamster_rival["hp"] > 0):
+
+time.sleep(1)
+while hamster_rival["hp"] > 0:
     attack()
-if hamster_rival["hp"] < 0:
+
+enemy_attack()
+print(HAMSTER["hp"])
+if HAMSTER["hp"] <= 0:
+    print("Game Over.")
+
+if hamster_rival["hp"] <= 0:
     print("You Defeated The Hamster Rival!")
